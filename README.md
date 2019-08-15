aed980adf171a56792770d3f75572641d0ef5705
Lillian Gales <lilliangales@ymail.com>
2019-08-14 21:23:45 -0700

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # lab3-async
+
+**Asynchronous code**
+
+The code is using using callbacks and promises to read and alter a json files content. In the callback function, it is sending the file and an external callback which gives access to the file string from line 14-24. In the case of no error, the file string is manipulated and sent back via the fs.writeFile function. When implemented with promises the process is similar, where the .then is acting as the "else" and reading and manipulating the data.  
--- edit-file.js
+++ edit-file.js
@@ -8,23 +8,28 @@ const myFileSystem = new FileSystem();
 
 console.log(__dirname);
 
+// Implemented with callbacks
+
 myFileSystem.readFile (`${__dirname}/data/person.json`,
     (error, fileString) => {
         if(error) {
             console.error('ERROR');
         } else {
             let obj = JSON.parse(fileString);
-            obj.firstName = 'Lill';
+            obj.firstName = 'Lillian';
             fs.writeFile(`${__dirname}/data/person.json`, JSON.stringify(obj), () => {
                 console.log('done');
             })
         }
     });
 
+// Implemented with Promises
+// Can't currently run at the same time
+
 myFileSystem.readFilePromises (`${__dirname}/data/person.json`)
     .then(fileString => {
         let obj = JSON.parse(fileString.toString());
-        obj.lastName = 'Ga';
+        obj.lastName = 'Gales';
         fs.writeFile(`${__dirname}/data/person.json`, JSON.stringify(obj), (error) => {
             console.log('done');
         })
